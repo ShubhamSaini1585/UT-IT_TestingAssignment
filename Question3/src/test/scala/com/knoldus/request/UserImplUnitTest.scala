f44c493ead403e4e71fb2600fab02e1c2360a4da
ShubhamSaini1585 <86958663+ShubhamSaini1585@users.noreply.github.com>
2021-09-14 22:16:30 +0530

    Update UserImplUnitTest.scala

--- Question3/src/test/scala/com/knoldus/request/UserImplUnitTest.scala
+++ Question3/src/test/scala/com/knoldus/request/UserImplUnitTest.scala
@@ -7,21 +7,21 @@ import org.scalatest.flatspec.AnyFlatSpec
 class UserImplUnitTest extends AnyFlatSpec {
 
   val mockedUserValidator = mock[UserValidator]
-  val lokeshUser: User = User("Lokesh","Kumar",21,"knoldus","lokesh.kumar@knoldus.com")
+  val shubhamUser: User = User("Shubham","Saini",23,"knoldus","shubham.saini@knoldus.com")
 
   "User" should "be created" in {
     val userImpl = new UserImpl(mockedUserValidator)
 
-    when(mockedUserValidator.userIsValid(lokeshUser)) thenReturn(true)
-    val result = userImpl.createUser(lokeshUser)
+    when(mockedUserValidator.userIsValid(shubhamUser)) thenReturn(true)
+    val result = userImpl.createUser(shubhamUser)
     assert(!result.isEmpty)
   }
 
   "User" should "not be created" in {
     val userImpl = new UserImpl(mockedUserValidator)
 
-    when(mockedUserValidator.userIsValid(lokeshUser)) thenReturn(false)
-    val result = userImpl.createUser(lokeshUser)
+    when(mockedUserValidator.userIsValid(shubhamUser)) thenReturn(false)
+    val result = userImpl.createUser(shubhamUser)
     assert(result.isEmpty)
   }
 
